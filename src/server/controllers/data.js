7458cdb43b942e93110d91f0d7727f0c3a60df88
Ryan "archaen" Pando <vretha@gmail.com>
2016-12-04 15:04:04 -0700

    Geocoding working.

--- src/server/controllers/data.js
+++ src/server/controllers/data.js
@@ -1,25 +1,66 @@
 const requestify = require('requestify');
 const knex = require('../db/connection');
+var geocoder = require('node-geocoder')({provider: 'google'});
 
-const url = 'http://www.civicdata.com/api/action/datastore_search?resource_id=d914e871-21df-4800-a473-97a2ccdf9690&limit=5';
 
-function getPermitData(){
+function getPermitData(record) {
+
+  const url = `http://www.civicdata.com/api/action/datastore_search_sql?sql=SELECT * from "d914e871-21df-4800-a473-97a2ccdf9690" WHERE _id > ${record} ORDER BY _id LIMIT 5`;
   return requestify.get(url);
 }
 
-function filterJson(data){
-  const filteredData = data.map(datum => {return { _id: datum._id, OriginalAddress: datum.OriginalCity, OriginalZip: datum.OriginalZip, AppliedDate: datum.AppliedDate, EstProjectCost: datum.EstProjectCost, LAT: datum.LAT, LON: datum.LON, Fee: datum.Fee
-  };});
-
-  return filteredData;
+function filterJson(data) {
+  return data.map(row => {
+    return {
+      _id: row._id,
+      OriginalCity: row.OriginalCity,
+      OriginalZip: row.OriginalZip,
+      AppliedDate: row.AppliedDate,
+      EstProjectCost: row.EstProjectCost,
+      LAT: row.LAT,
+      LON: row.LON,
+      Fee: row.Fee
+    };
+  });
 }
 
 function insertPermitData(data) {
   return knex('permits').insert(data);
 }
 
+function geoCode(data) {
+  let promises = data.map(row => {
+    if (row.LAT || row.LON) {
+      return geocoder.reverse({lat: row.LAT, lon: row.LON});
+    }
+    else {
+      return null;
+    }
+  });
+
+  return Promise.all(promises).then(location => {
+    const updatedGeo = data.map((row, index) => {
+      if (row.OriginalCity === '' && row.LAT) {
+        row.OriginalCity = location[index][0].city;
+        row.OriginalZip = location[index][0].zipcode;
+        return row;
+      }
+      else {
+        return row;
+      }
+    });
+    return updatedGeo;
+  });
+}
+
+function getLastRecord() {
+  return knex('permits').max('_id');
+}
+
 module.exports = {
   getPermitData,
   filterJson,
-  insertPermitData
+  insertPermitData,
+  geoCode,
+  getLastRecord
 };
--- src/server/routes/data.js
+++ src/server/routes/data.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const geocoder = require('geocoder');
 
 const dataController = require('../controllers/data.js');
 
 router.get('/updatePermitData', function (req, res, next) {
-  const promise = dataController.getPermitData()
-  .then(function(response) {
-      const result = response.getBody().result;
-      const filteredJson = dataController.filterJson(result.records);
-      const psqlResponse = dataController.insertPermitData(filteredJson);
-      psqlResponse.then(response => res.json(response));
-  }).catch(err => next(err));
+  dataController.getLastRecord()
+  .then(record => {
+    const promise = dataController.getPermitData(record[0].max)
+    .then(function(response) {
+        const result = response.getBody().result;
+        const filteredJson = dataController.filterJson(result.records);
+        dataController.geoCode(filteredJson)
+        .then(geoCoded => {
+          dataController.insertPermitData(geoCoded)
+          .then(response => res.json(response));
+        });
+    }).catch(err => next(err));
+  }
+
+  );
+
 
 
 });
